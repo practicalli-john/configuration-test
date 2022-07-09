d58959dc272882116ebe400fed9e5fe4a96f913d
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-07-09 19:00:41 +0100

    New project from app template
    Add GitHub action workflow to run clj-kondo, cljstyle and kaocha test
    runner

---
+++ test/practicalli/config_testing_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.config-testing-test
+  (:require [clojure.test :refer :all]
+            [practicalli.config-testing :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
