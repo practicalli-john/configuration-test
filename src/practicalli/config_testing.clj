b5c8774d33f01dc6d4fa4ed3c8066fd9b29062e5
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-07-10 01:50:53 +0100

    code: remove blank lines between forms to generate style diff

--- src/practicalli/config_testing.clj
+++ src/practicalli/config_testing.clj
@@ -1,13 +1,9 @@
 (ns practicalli.config-testing
   (:gen-class))
-
-
 (defn greet
   "Callable entry point to the application."
   [data]
   (println (str "Hello, " (or (:name data) "World") "!")))
-
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
